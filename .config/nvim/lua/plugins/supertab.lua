7335a6dfedd8913de32c8b6c6adc1e4c11c29e8c
Hugo Ribeira <hugoribeira@gmail.com>
2025-09-04 16:59:44 +0100

    Add more auto-complete engines

--- .config/nvim/lua/plugins/supertab.lua
+++ .config/nvim/lua/plugins/supertab.lua
@@ -4,6 +4,9 @@ return {
   opts = {},
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-calc",
   },
   config = function()
     local cmp = require('cmp')
@@ -40,7 +43,9 @@ return {
 
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
-        { name = "buffer" }
+        { name = "buffer" },
+        { name = "path" },
+        { name = "calc" },
       }),
 
       window = {
